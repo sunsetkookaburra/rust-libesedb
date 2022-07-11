7f40cb4361b5de2142b20156ccae4a2f969db827
Oliver Lenehan <55334808+sunsetkookaburra@users.noreply.github.com>
2022-07-11 11:07:58 +1000

    fix: builds on unix (#3)
    Co-authored-by: Jan Starke <7724055+janstarke@users.noreply.github.com>

--- libesedb-sys/build.rs
+++ libesedb-sys/build.rs
@@ -33,27 +33,38 @@ fn main() {
         writeln!(page_tree_new, "{line}").unwrap();
         if line.starts_with("#include \"esedb_page_values.h\"") {
             writeln!(page_tree_new).unwrap();
-            writeln!(
-                page_tree_new,
-                r#"
-                    #undef LIBESEDB_MAXIMUM_NUMBER_OF_LEAF_PAGES
-                    #define LIBESEDB_MAXIMUM_NUMBER_OF_LEAF_PAGES   ({max_leaf_pages})
-                "#
-            )
-            .unwrap();
+            writeln!(page_tree_new, "#undef LIBESEDB_MAXIMUM_NUMBER_OF_LEAF_PAGES").unwrap();
+            writeln!(page_tree_new, "#define LIBESEDB_MAXIMUM_NUMBER_OF_LEAF_PAGES   ({max_leaf_pages})").unwrap();
         }
     }
     drop(page_tree_new);
 
-    c.file(&page_tree);
     c.include("libesedb-20210424/include");
     c.include("libesedb-20210424/common");
+    c.include("libesedb-20210424/libbfio");
+    c.include("libesedb-20210424/libcdata");
+    c.include("libesedb-20210424/libcerror");
+    c.include("libesedb-20210424/libcfile");
+    c.include("libesedb-20210424/libclocale");
+    c.include("libesedb-20210424/libcnotify");
+    c.include("libesedb-20210424/libcpath");
+    c.include("libesedb-20210424/libcsplit");
+    c.include("libesedb-20210424/libcthreads");
+    c.include("libesedb-20210424/libesedb");
+    c.include("libesedb-20210424/libfcache");
+    c.include("libesedb-20210424/libfdata");
+    c.include("libesedb-20210424/libfdatetime");
+    c.include("libesedb-20210424/libfguid");
+    c.include("libesedb-20210424/libfmapi");
+    c.include("libesedb-20210424/libfvalue");
+    c.include("libesedb-20210424/libfwnt");
+    c.include("libesedb-20210424/libmapidb");
+    c.include("libesedb-20210424/libuna");
 
     for ent in WalkDir::new("libesedb-20210424") {
         let ent = ent.unwrap();
-        let typ = ent.file_type();
-        let name = ent.file_name().to_str().unwrap();
-        if typ.is_file() {
+        if ent.file_type().is_file() {
+            let name = ent.file_name().to_str().unwrap();
             if name.ends_with(".c") {
                 let parent_name = ent
                     .path()
@@ -64,36 +75,37 @@ fn main() {
                     .to_str()
                     .unwrap();
                 if name == "libesedb_page_tree.c" {
-                    continue;
+                    c.file(&page_tree);
                 } else if parent_name.starts_with("lib") {
                     c.file(ent.path());
                 }
             }
-        } else if typ.is_dir() {
-            if name.starts_with("lib") {
-                c.include(ent.path());
-            }
         }
     }
 
-    c.define("HAVE_LOCAL_LIBBFIO", "1");
-    c.define("HAVE_LOCAL_LIBCDATA", "1");
-    c.define("HAVE_LOCAL_LIBCERROR", "1");
-    c.define("HAVE_LOCAL_LIBCFILE", "1");
-    c.define("HAVE_LOCAL_LIBCLOCALE", "1");
-    c.define("HAVE_LOCAL_LIBCNOTIFY", "1");
-    c.define("HAVE_LOCAL_LIBCPATH", "1");
-    c.define("HAVE_LOCAL_LIBCSPLIT", "1");
-    c.define("HAVE_LOCAL_LIBCTHREADS", "1");
-    c.define("HAVE_LOCAL_LIBFCACHE", "1");
-    c.define("HAVE_LOCAL_LIBFDATA", "1");
-    c.define("HAVE_LOCAL_LIBFDATETIME", "1");
-    c.define("HAVE_LOCAL_LIBFGUID", "1");
-    c.define("HAVE_LOCAL_LIBFMAPI", "1");
-    c.define("HAVE_LOCAL_LIBFVALUE", "1");
-    c.define("HAVE_LOCAL_LIBFWNT", "1");
-    c.define("HAVE_LOCAL_LIBMAPIDB", "1");
-    c.define("HAVE_LOCAL_LIBUNA", "1");
+    if cfg!(target_family = "unix") {
+        c.define("HAVE_CONFIG_H", None);
+        c.define("LOCALEDIR", "\"/usr/share/locale\"");
+    } else if cfg!(target_family = "windows") {
+        c.define("HAVE_LOCAL_LIBBFIO", "1");
+        c.define("HAVE_LOCAL_LIBCDATA", "1");
+        c.define("HAVE_LOCAL_LIBCERROR", "1");
+        c.define("HAVE_LOCAL_LIBCFILE", "1");
+        c.define("HAVE_LOCAL_LIBCLOCALE", "1");
+        c.define("HAVE_LOCAL_LIBCNOTIFY", "1");
+        c.define("HAVE_LOCAL_LIBCPATH", "1");
+        c.define("HAVE_LOCAL_LIBCSPLIT", "1");
+        c.define("HAVE_LOCAL_LIBCTHREADS", "1");
+        c.define("HAVE_LOCAL_LIBFCACHE", "1");
+        c.define("HAVE_LOCAL_LIBFDATA", "1");
+        c.define("HAVE_LOCAL_LIBFDATETIME", "1");
+        c.define("HAVE_LOCAL_LIBFGUID", "1");
+        c.define("HAVE_LOCAL_LIBFMAPI", "1");
+        c.define("HAVE_LOCAL_LIBFVALUE", "1");
+        c.define("HAVE_LOCAL_LIBFWNT", "1");
+        c.define("HAVE_LOCAL_LIBMAPIDB", "1");
+        c.define("HAVE_LOCAL_LIBUNA", "1");
+    }
 
     c.compile("esedb");
 }
